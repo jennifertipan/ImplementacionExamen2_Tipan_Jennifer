4eb3151f0cf2b1aab77c3ed2a0fcc893f12c0dde
TECNOMANIA <jetipan5@espe.edu.ec>
2021-08-11 18:42:48 -0500

    Jubilacion

---
+++ src/dominio/JubiladoPatron.java
@@ -0,0 +1,35 @@
+
+package dominio;
+
+
+public class JubiladoPatron extends Jubilado implements Finanzas{
+    private double inflacion;
+    private int tipoe;
+    private double pensionp;
+    
+    public JubiladoPatron(String cedula, String nombres, int aniosAporte,double inflacion,int tipoe) {
+        super(cedula, nombres, aniosAporte);
+        this.inflacion = inflacion;
+        this.tipoe = tipoe;
+    }
+
+    @Override
+    public void calcularPension() {
+        this.pensionp = getPensioni()+(getSALARIOBASE()*inflacion);
+    }
+
+    @Override
+    public void bonoSueldo() {
+        if(this.tipoe ==1){
+            this.pensionp = this.pensionp*1.05;
+        }else{
+            this.pensionp = this.pensionp*1.10;
+        }
+    }
+    @Override
+    public String toString(){
+        return super.toString()+"\nTipo de empresa:  "+this.tipoe+"\nPensión a recibir:  "+this.pensionp;
+    }
+    
+    
+}
